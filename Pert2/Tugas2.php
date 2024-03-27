3cd0ea734859d45e78971aab0b3cb7ad8c4f1eee
SandykaDK <22410100059@dinamika.ac.id>
2024-03-27 21:35:49 +0700

    revisi

--- Pert2/Tugas2.php
+++ Pert2/Tugas2.php
@@ -2,16 +2,17 @@
 
     <head>
         <title> TUGAS 2 </title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="mycss.css">
-        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <a href="TambahBarang2.php" target="_blank">  
-            <input type="button" name="Tambah" value="Tambah">
-        </a>
 
-        
+        <a href="TambahBarang2.php" target="blank">  
+            <button type="button" class="buttontambah">
+                <i class="fas fa-sync icon"></i>
+            </button>
+        </a>
 
         <br></br>
 
@@ -65,6 +66,21 @@
                 <td>
                     <a href="EditBarang2.php" target="_blank">
                         <input type="button" name="Edit" value="Edit">
+                    </a>
+                </td>		
+            </tr>
+
+            <tr>
+                <td>M03</td>
+                <td>VGA</td>
+                <td>pcs</td>
+                <td>400</td>
+                <td>550</td>
+                <td>
+                    <a href="EditBarang2.php" target="_blank">  
+                        <button type="button" class="buttonEdit">
+                            <i class="fas fa-sync icon"></i> Edit
+                        </button>
                     </a>
                 </td>		
             </tr>
