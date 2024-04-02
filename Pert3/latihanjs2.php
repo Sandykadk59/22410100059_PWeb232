fe1d303d7ce221f0741a38fc72073795c46cdeb7
SandykaDK <22410100059@dinamika.ac.id>
2024-04-02 08:46:20 +0700

    Pert3

---
+++ Pert3/latihanjs2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <p id="isi"> isi </p>
+    <p> mantap benar </p>
+    <button type="button" onclick="myfungsi()"> Klik </button>
+    <button type="button" onclick="myfungsi1()"> Klik </button>
+
+    <script>
+        document.getElementById("isi").innerHTML = 5 + 6;
+
+        document.write(5+12);
+        document.write("\n sip");
+
+        function myfungsi(){
+            window.alert("ERROR 404");
+        }
+
+        function myfungsi1(){
+            console.log("Ini muncul di log");
+        }
+
+    </script>
+</body>
+</html>
