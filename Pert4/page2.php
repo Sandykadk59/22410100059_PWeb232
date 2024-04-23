fdd71296f5ad3f1125c8b4adaf50b0a184533492
SandykaDK <22410100059@dinamika.ac.id>
2024-04-23 12:46:22 +0700

    Latihan Pert 4

---
+++ Pert4/page2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+
+</head>
+<body>
+    <h1> Tambah Barang </h1>
+    <form>
+        <table border="1">
+            <tr><td> Kode </td><td><input type="text" name="kode" id="kode"></td></tr>
+            <tr><td> Nama </td><td><input type="text" name="nama" id="nama"></td></tr>
+            <tr><td> Satuan </td><td><input type="text" name="satuan" id="satuan"></td></tr>
+            <tr><td> Harga Beli </td><td><input type="text" name="hargabeli" id="hargabeli"></td></tr>
+            <tr><td> Jual </td><td><input type="text" name="hargajual" id="hargajual"></td></tr>
+        </table>
+
+        <button id="save"> Save </button>
+        <button id="cancel"> Cancel </button>
+    </form>
+</body>
+</html>
